32f76bb0af9700b82108b36961118e2170e8956f
David Ankenbrand <Ank19415@spengergasse.at>
2022-10-05 14:36:21 +0200

    delete Warehouse Unittest

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.Test/UnitTest1.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Test/UnitTest1.cs
@@ -728,64 +728,7 @@ namespace Spg.AutoTeileShop.Test
             Assert.Equal(0, db.Cars.First().FitsForProducts.Count());
             Assert.Equal(0, db.Products.First().ProductFitsForCar.Count());
         }
-        //[Fact]
-        //public void Create_Warehouse()
-        //{
-
-        //    AutoTeileShopContext db = createDB();
-
-        //    Warehouse warehouse = new Warehouse()
-        //    {
-        //        Guid = Guid.NewGuid(),
-        //    };
-
-        //    db.Warehouses.Add(warehouse);
-
-        //    db.SaveChanges();
-        //    Assert.Equal(1, db.Warehouses.Count());
-
-
-        //}
-
-        //[Fact]
-        //public void Create_Warehouse_with_Product()
-        //{
-
-        //    AutoTeileShopContext db = createDB();
-
-        //    Product product = new Product()
-        //    {
-        //        Description = "Des Test",
-        //        Guid = Guid.NewGuid(),
-        //        Name = "Pro Test",
-        //        Price = 499.99M
-        //    };
-        //    db.Products.Add(product);
-
-        //    Product product2 = new Product()
-        //    {
-        //        Description = "Des Test2",
-        //        Guid = Guid.NewGuid(),
-        //        Name = "Pro Test2",
-        //        Price = 499.99M
-        //    };
-        //    db.Products.Add(product2);
-
-        //    Warehouse warehouse = new Warehouse()
-        //    {
-        //        Guid = Guid.NewGuid(),
-        //    };
-        //    warehouse.AddProduct(product);
-        //    warehouse.AddProduct(product2);
-
-        //    db.Warehouses.Add(warehouse);
-        //    db.SaveChanges();
-
-        //    Assert.Equal(1, db.Warehouses.Count());
-        //    Assert.Equal(2, db.Products.Count());
-        //    Assert.Equal(product, db.Warehouses.Find(warehouse.Id).Products.First());
-        //    Assert.Equal(product2, db.Warehouses.Find(warehouse.Id).Products.Last());
-        //}
+        
 
     }
 }
