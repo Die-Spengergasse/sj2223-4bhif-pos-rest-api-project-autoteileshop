8ceaf03a6b0bc172428f04e3d28bbc8ef7f10f4f
David Ankenbrand <Ank19415@spengergasse.at>
2022-12-24 16:33:13 +0100

    send Mail Class add

---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Application/SendMail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.Application
+{
+    public class SendMail
+    {
+
+        public string Send(string Acc, string emailFrom, string emailTo, string emailSubject, string emailBody)
+        {
+            string guidString = Guid.NewGuid().ToString().Substring(0, 8);
+
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(emailFrom, "bppkrfqjnqxvthxl"),
+                EnableSsl = true
+            };
+            if (emailSubject.Count() == 0)
+            {
+                emailSubject = "Anmelde Bestätigung SpengerCars";
+            }
+            if (emailBody.Count() == 0)
+            { emailBody = "Dies ist die Bestätigung für die Anmeldung des Accounts " + Acc + " ,\r\nBitte geben sie diesen Code: " + guidString + " zur Bestätigung ein"; }
+            client.Send(emailFrom, emailTo, emailSubject, emailBody);
+            return guidString;
+        }
+    }
+}
