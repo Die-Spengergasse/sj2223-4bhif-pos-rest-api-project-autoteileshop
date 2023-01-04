7f67d9af536a4e4126394c4d905da89ef2a1cdad
David Ankenbrand <Ank19415@spengergasse.at>
2023-01-04 18:44:51 +0100

    Car Controller

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Controllers/CarController.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Controllers/CarController.cs
@@ -31,7 +31,7 @@ namespace Spg.AutoTeileShop.API.Controllers
             return Ok(_readOnlycarService.GetAll());
         }
 
-
+               
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -52,8 +52,69 @@ namespace Spg.AutoTeileShop.API.Controllers
             }
         }
 
+        [HttpGet("GetByBaujahr/{year}")]
+        public ActionResult<List<Car>> GetByBaujahr(int year)
+        {
+            try
+            {
+                return Ok(_readOnlycarService.GetByBauJahr(new DateTime(year, 1, 1)));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
 
+        [HttpGet("GetByMarke/{marke}")]
+        public ActionResult<List<Car>> GetByMarke(string marke)
+        {
+            try
+            {
+                return Ok(_readOnlycarService.GetByMarke(marke));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("GetByModell/{model}")]
         public ActionResult<List<Car>> GetByModell(string model)
+        {
+            try
+            {
+                return Ok(_readOnlycarService.GetByModell(model));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("GetByMarkeAndModell/{marke}/{model}")]
+        public ActionResult<List<Car>> GetByMarkeAndModell(string marke, string model)
+        {
+            try
+            {
+                return Ok(_readOnlycarService.GetByMarkeAndModell(marke, model));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("GetByMarkeAndModellAndBaujahr/{merke}/{model}/{baujahr}")]
         public ActionResult<List<Car>> GetByMarkeAndModellAndBaujahr(string merke, string model, int baujahr)
+        {
+            try
+            {
+                return Ok(_readOnlycarService.GetByMarkeAndModellAndBaujahr(merke, model, new DateTime(baujahr, 1, 1)));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
     }
 }
