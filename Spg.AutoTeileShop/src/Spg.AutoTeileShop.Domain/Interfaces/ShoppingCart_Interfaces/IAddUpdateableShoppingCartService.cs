4cc26ca65fb9c13919d4879f66ca3424eb2bb1af
David Ankenbrand <Ank19415@spengergasse.at>
2023-01-11 17:46:08 +0100

    ShoppingCartService: IAddUpdateable, IDeleteable, IReadOnly

---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Domain/Interfaces/ShoppingCart_Interfaces/IAddUpdateableShoppingCartService.cs
@@ -0,0 +1,16 @@
+﻿using Spg.AutoTeileShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.Domain.Interfaces.ShoppingCart_Interfaces
+{
+    public interface IAddUpdateableShoppingCartService
+    {
+        ShoppingCart AddShoppingCart(ShoppingCart shoppingCart);
+        ShoppingCart UpdateShoppingCart(ShoppingCart item);
+
+    }
+}
