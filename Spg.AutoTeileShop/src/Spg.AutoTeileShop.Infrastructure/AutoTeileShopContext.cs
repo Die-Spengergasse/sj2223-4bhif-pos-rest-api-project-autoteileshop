8775ad38f94f155d7c9fb4f7d0b2261f5369606a
HannesOnGit <97890806+HannesOnGit@users.noreply.github.com>
2023-01-12 15:01:36 +0100

    path changed

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.Infrastructure/AutoTeileShopContext.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Infrastructure/AutoTeileShopContext.cs
@@ -32,7 +32,8 @@ namespace Spg.AutoTeileShop.Infrastructure
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             if (!options.IsConfigured)
-                options.UseSqlite("Data Source= I:\\Dokumente 4TB\\HTL\\4 Klasse\\POS1 Git Repo\\sj2223-4bhif-pos-rest-api-project-autoteileshop\\Spg.AutoTeileShop\\src\\AutoTeileShop.db"); //Home PC
+                options.UseSqlite("DataSource=C:\\Users\\User\\source\\repos\\sj2223-4bhif-pos-rest-api-project-autoteileshop\\Spg.AutoTeileShop\\src\\AutoTeileShop.db");
+                    //"I:\\Dokumente 4TB\\HTL\\4 Klasse\\POS1 Git Repo\\sj2223-4bhif-pos-rest-api-project-autoteileshop\\Spg.AutoTeileShop\\src\\AutoTeileShop.db"); //Home PC
                             //  D:/4 Klasse/Pos1 Repo/sj2223-4bhif-pos-rest-api-project-autoteileshop/Spg.AutoTeileShop/src/AutoTeileShop.db"     //Laptop
             
             
