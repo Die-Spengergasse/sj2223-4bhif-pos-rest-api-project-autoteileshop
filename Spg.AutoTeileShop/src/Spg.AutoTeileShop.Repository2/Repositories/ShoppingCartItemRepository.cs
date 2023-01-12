4bc8bc313f4fa43f89b1228181c111cd6528521d
HannesOnGit <97890806+HannesOnGit@users.noreply.github.com>
2023-01-12 15:23:25 +0100

    shoppingcart  repo

---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Repository2/Repositories/ShoppingCartItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using Spg.AutoTeileShop.Domain.Interfaces.ShoppingCartItem_Interface;
+using Spg.AutoTeileShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.Repository2.Repositories
+{
+    public class ShoppingCartItemRepository : IShoppingCartItemRepository
+    {
+        public ShoppingCartItem Add(ShoppingCartItem shoppingCartItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCartItem Delete(ShoppingCartItem shoppingCartItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCartItem GetByGuid(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCartItem GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCartItem Update(ShoppingCartItem shoppingCartItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
