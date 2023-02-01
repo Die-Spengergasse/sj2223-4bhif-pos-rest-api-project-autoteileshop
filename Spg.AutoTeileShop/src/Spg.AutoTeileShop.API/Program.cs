83afd0f120fe4889f371a75c536e99470a2700ba
David Ankenbrand <Ank19415@spengergasse.at>
2023-02-01 23:10:08 +0100

    stupid

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Program.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Program.cs
@@ -21,7 +21,7 @@ using System.Reflection;
 //product get del -id
 //ShoppingCart del put guid
 //ShoppingCartItem get ShoppingCar
-//var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
