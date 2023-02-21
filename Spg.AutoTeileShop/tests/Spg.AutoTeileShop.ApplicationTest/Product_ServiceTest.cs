77bd41db7fda5e1762a6036ca79bdf6d69caea4a
David-vie21 <david.ankenbrand98@gmail.com>
2023-02-21 15:58:44 +0100

    product Repo Test

---
+++ Spg.AutoTeileShop/tests/Spg.AutoTeileShop.ApplicationTest/Product_ServiceTest.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Spg.AutoTeileShop.Application.Services;
+using Spg.AutoTeileShop.Domain.DTO;
+using Spg.AutoTeileShop.Domain.Interfaces.ProductServiceInterfaces;
+using Spg.AutoTeileShop.Domain.Models;
+using Spg.AutoTeileShop.Infrastructure;
+using Spg.AutoTeileShop.Repository2.Repositories;
+
+namespace Spg.AutoTeileShop.ApplicationTest
+{
+    public class Product_ServiceTest
+    {
+        private AutoTeileShopContext createDB()
+        {
+            DbContextOptions options = new DbContextOptionsBuilder()
+                  //.UseSqlite("Data Source=AutoTeileShopTest.db")
+                  //.UseSqlite(@"Data Source= D:/4 Klasse/Pos1 Repo/sj2223-4bhif-pos-rest-api-project-autoteileshop/Spg.AutoTeileShop/src/AutoTeileShop.db")      //Laptop
+                  .UseSqlite(@"Data Source = I:\Dokumente 4TB\HTL\4 Klasse\POS1 Git Repo\sj2223-4bhif-pos-rest-api-project-autoteileshop\Spg.AutoTeileShop\src\Spg.AutoTeileShop.API\db\AutoTeileShop.db")     //Home PC       
+                .Options;
+
+            AutoTeileShopContext db = new AutoTeileShopContext(options);
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            db.Seed();
+            return db;
+        }
+
+        [Fact]
+        public void Create_Succes_Test()
+        {
+            //Arrange
+            //Datenbank
+            AutoTeileShopContext db = createDB();
+            
+            ProductService productService = new(new ProductRepository(db));
+            Product product = new Product()
+            {
+                Ean13 = "dagasgasf",
+                Description = "Des Test",
+                Guid = Guid.NewGuid(),
+                Name = "Pro Test",
+                Price = 499.99M,
+                Stock = 1               
+            };
+
+            //Act
+
+            productService.Add(product);
+
+            //Assert
+
+            Assert.Single(db.Products.ToList());
+        }
+    }
+}
