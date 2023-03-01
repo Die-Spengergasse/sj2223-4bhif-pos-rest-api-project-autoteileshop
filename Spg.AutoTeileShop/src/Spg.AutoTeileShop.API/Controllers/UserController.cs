bd5fe276096b9a74b0c88dc1b91be28b02192c99
David Ankenbrand <Ank19415@spengergasse.at>
2023-03-01 16:37:13 +0100

    changed DOTNET Version from 6.0 to 7.0 - and NugetPackeges

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Controllers/UserController.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Controllers/UserController.cs
@@ -66,7 +66,7 @@ namespace Spg.AutoTeileShop.API.Controllers
             return Ok(new UserGetDTO(response));
         }
 
-        [HttpDelete("/{guid}")]
+        [HttpDelete("{guid}")]
         public ActionResult<User> DeleteUserByGuid(Guid guid)
         {
             try
@@ -89,7 +89,7 @@ namespace Spg.AutoTeileShop.API.Controllers
             }
         }
 
-        [HttpPut("/{guid}")]
+        [HttpPut("{guid}")]
         public ActionResult<User> UpdateUser([FromBody()] UserUpdateDTO userJSON, Guid guid)
         {
             try
