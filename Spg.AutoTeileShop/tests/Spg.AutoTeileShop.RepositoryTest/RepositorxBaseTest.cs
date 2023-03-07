dec551176e32180415f09b352417a4e885bd748b
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-03-07 14:54:40 +0100

    dotnet7 ,Database Utilities and Gerneric Service Tests - not run able

---
+++ Spg.AutoTeileShop/tests/Spg.AutoTeileShop.RepositoryTest/RepositorxBaseTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.RepositoryTest
+{
+    public class RepositorxBaseTest
+    {
+        //private AutoTeileShopContext GetDB()
+        //{
+        //    SqliteConnection connection = new SqliteConnection("DataSource=:memory:");
+        //    connection.Open();
+
+        //    return new DbContextOptionsBuild()
+        //        .UseSqlite(connection)
+        //        .Options;
+        //}
+
+    }
+  
+    
+}
