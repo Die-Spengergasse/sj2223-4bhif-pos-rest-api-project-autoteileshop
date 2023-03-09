bc9ee727e41e537e43a46b8b8237cc6ad61c266c
David Ankenbrand <Ank19415@spengergasse.at>
2023-03-09 20:40:32 +0100

    Add Interfaces

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.Domain/Interfaces/RepoBase_Interfaces/IReadOnlyRepositoryBase.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Domain/Interfaces/RepoBase_Interfaces/IReadOnlyRepositoryBase.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace Spg.AutoTeileShop.Domain.Interfaces.RepoBase_Interfaces
 {
-    internal interface IRepositoryBasey<TEntity> where TEntity : class
+    public interface IReadOnlyRepositoryBase<TEntity> where TEntity : class
     {
-        TEntity? Create(TEntity entity);
-        TEntity? Update(TEntity entity);
-        TEntity? Delete(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        TEntity? GetById(int Id);
     }
 }
