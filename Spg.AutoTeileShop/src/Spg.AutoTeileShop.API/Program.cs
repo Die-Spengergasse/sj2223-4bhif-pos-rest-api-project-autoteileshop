1c757f86a979bb0c492bd45c87cec186a03ee39b
David Ankenbrand <Ank19415@spengergasse.at>
2023-04-14 18:00:05 +0200

    init - api/ - api/v1 - api/v2 links as json, BUG: href dosnt work

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Program.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.API/Program.cs
@@ -1,8 +1,14 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Org.BouncyCastle.Asn1.Cmp;
 using Spg.AutoTeileShop.Application.Services;
 using Spg.AutoTeileShop.Application.Validators;
 using Spg.AutoTeileShop.DbExtentions;
@@ -17,6 +23,11 @@ using Spg.AutoTeileShop.Repository2.Repositories;
 using Spg.AutoTeileShop.ServiceExtentions;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Web.Helpers;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
 
 //product get del -id
 //ShoppingCart del put guid
@@ -37,6 +48,19 @@ builder.Services.ConfigureSQLite(connectionString);
 
 builder.Services.AddControllers();
 
+// Link Generator
+//builder.Services.AddSingleton<LinkGenerator>(new LinkGenerator(
+//    new ActionContext 
+//    {
+//        HttpContext = new DefaultHttpContext(),
+//        RouteData = new RouteData(),
+//        ActionDescriptor = new ActionDescriptor(),
+
+//    },
+//    new List<IEndpointRouteBuilder>(),
+//    new OptionsManager<RouteOptions>(Enumerable.Empty<IConfigureOptions<RouteOptions>>())
+//    ));
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -44,28 +68,10 @@ builder.Services.AddSwaggerGen(s =>
     s.ResolveConflictingActions(apiDescriptions => apiDescriptions.First())
     );
 
-// NuGet: Microsoft.AspNetCore.Mvc.Versioning
-builder.Services.AddApiVersioning(o =>
-{
-    o.AssumeDefaultVersionWhenUnspecified = true;
-    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(2, 0);
-    o.ReportApiVersions = true;
-    o.ApiVersionReader = ApiVersionReader.Combine(
-        new QueryStringApiVersionReader("api-version"),
-        new HeaderApiVersionReader("X-Version"),
-        new MediaTypeApiVersionReader("ver"));
-});
-builder.Services.AddVersionedApiExplorer(
-    options =>
-    {
-        options.GroupNameFormat = "'v'VVV";
-        options.SubstituteApiVersionInUrl = true;
-    });
-
 
 builder.Services.AddSwaggerGen(s =>
 {
-    
+
     s.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo()
     {
         Title = "AutoTeile Shop - v2",
@@ -131,6 +137,26 @@ builder.Services.AddSwaggerGen(s =>
 
 
 
+// NuGet: Microsoft.AspNetCore.Mvc.Versioning
+builder.Services.AddApiVersioning(o =>
+{
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(2, 0);
+    o.ReportApiVersions = true;
+    o.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("X-Version"),
+        new MediaTypeApiVersionReader("ver"));
+});
+builder.Services.AddVersionedApiExplorer(
+    options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
+
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "myAllowSpecificOrigins", policy =>
@@ -155,9 +181,96 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseCors("myAllowSpecificOrigins");
-
 app.MapControllers();
+//LinkGenerator linkGenerator = app.Services.GetRequiredService<LinkGenerator>();
+//string? url = linkGenerator.GetUriByAction(HttpContext, action: nameof(GetDetails), values: new { name = newId });
+// Json json = new Json();
 
+
+var linksv1 = new List<string>();
+//var controllerNameList = new List<string>();
+
+//var endpoints = app.Services.GetService<EndpointDataSource>();
+//foreach (var endpoint in endpoints.Endpoints)
+//{
+//    var controllerEndpoint = endpoint.Metadata.GetMetadata<ControllerActionDescriptor>();
+//    if (controllerEndpoint != null)
+//    {
+//        var controllerName = controllerEndpoint.ControllerName;
+//        controllerNameList.Add(controllerName);
+//    }
+//}
+//links.Add(builder.Services.);
+
+
+//var asm = Assembly.GetAssembly(typeof(Program));
+//var controlleractionlist = asm.GetTypes()
+//    .Where(type => typeof(Controllerv1).IsAssignableFrom(type))
+//    .SelectMany(type => type.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public))
+//    .Where(m => !m.GetCustomAttributes(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), true).Any())
+//    .Select(x => new
+//    {
+//        Controllerv1 = x.DeclaringType.Name,
+//        Action = x.Name,
+//        ReturnType = x.ReturnType.Name,
+//        Attributes = String.Join(",", x.GetCustomAttributes().Select(a => a.GetType().Name.Replace("Attribute", "")))
+//    })
+//    .OrderBy(x => x.Controllerv1).ThenBy(x => x.Action).ToList();
+
+//var list = new List<String>();
+//foreach (var item in controlleractionlist)
+//{
+//    list.Add(item.Controllerv1);
+//}
+
+
+//var asm = Assembly.GetAssembly(typeof(Program));
+//var controllerTypes = asm.GetTypes().Where(type => typeof(Controllerv1).IsAssignableFrom(type));
+//var controllerNames = controllerTypes.Select(type => type.Name);
+var assembly = typeof(Program).Assembly;
+var controllerTypes = assembly.GetTypes().Where(type => typeof(ControllerBase).IsAssignableFrom(type) && !type.IsAbstract);
+
+var controllerNames = controllerTypes.Select(x => x.Name.Replace("Controller", "")).ToList();
+
+for (int i = 0; i <= (controllerNames.Count / 2) - 1; i++)
+{
+    linksv1.Add("https://localhost:7083/api/v1/" + controllerNames[i]);
+}
+
+var linksv2 = new List<string>();
+
+for (int i = 0; i <= (controllerNames.Count / 2) - 1; i++)
+{
+    linksv2.Add("https://localhost:7083/api/v2/" + controllerNames[i]);
+}
+//links.Add();
+//foreach (string item in controllerNames)
+//{
+//    links.Add("https://localhost:7083/api/" + item);
+//}
+
+var Controllerv1 = new { Controller = linksv1 };
+var Controllerv2 = new { Controller = linksv2 };
+
+
+var jsonsConv1 = JsonConvert.SerializeObject(Controllerv1, Formatting.Indented);
+var jsonsConv2 = JsonConvert.SerializeObject(Controllerv2, Formatting.Indented);
+
+
+
+var versionList = new List<string>
+{
+    "https://localhost:7083/api/v1",
+    "https://localhost:7083/api/v2"
+};
+
+
+var jsonsVersion = JsonConvert.SerializeObject(versionList, Formatting.Indented);
+
+
+app.MapGet("/api/", () => jsonsVersion);
+app.MapGet("/api/v1", () => jsonsConv1);
+app.MapGet("/api/v2", () => jsonsConv2);
 app.Run();
 
 
