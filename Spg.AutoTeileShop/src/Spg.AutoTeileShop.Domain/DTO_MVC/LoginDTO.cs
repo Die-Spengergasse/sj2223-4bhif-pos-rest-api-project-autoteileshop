204c17a1f3e29131c552776a0f71e13af991659d
David Ankenbrand <david.ankenbrand98@gmail.com>
2023-05-09 15:59:14 +0200

    Login View without logic, but Dotnet Versions are fucked up

---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Domain/DTO_MVC/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.Domain.DTO_MVC
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+    }
+}
---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.MVCFrontEnd/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spg.AutoTeileShop.Domain.DTO_MVC;
+
+namespace Spg.AutoTeileShop.MVCFrontEnd.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            string userName = HttpContext.Request.Cookies["User"] ?? "not loged in";
+            return View("Login", userName);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDTO dto)
+        {
+            HttpContext.Response.Cookies.Append("User", dto.Username);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
