9ee16737f9534bbd5fb59c7edb99ba6c31b6d3fd
David Ankenbrand <Ank19415@spengergasse.at>
2023-06-01 17:45:17 +0200

    Moved Mediator to right Place

--- Spg.AutoTeileShop/src/Spg.AutoTeileShop.Application/Services/CQS/Mediator.cs
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Application/Services/CQS/Mediator.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Spg.AutoTeileShop.Application.Services.CQS.Car
+namespace Spg.AutoTeileShop.Application.Services.CQS
 {
     public class Mediator : IMediator
     {
