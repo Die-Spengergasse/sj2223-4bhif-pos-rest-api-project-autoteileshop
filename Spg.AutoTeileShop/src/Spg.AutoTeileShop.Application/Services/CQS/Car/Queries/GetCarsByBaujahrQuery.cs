b3e71704ce0a6e4b9c462990e89331bf1e5c0836
David Ankenbrand <Ank19415@spengergasse.at>
2023-06-01 19:20:58 +0200

    Add Car Custom Repo

---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Application/Services/CQS/Car/Queries/GetCarsByBaujahrQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.Application.Services.CQS.Car.Queries
+{
+    public class GetCarsByBaujahrQuery : IRequest<IEnumerable<Spg.AutoTeileShop.Domain.Models.Car>>
+    {
+        public DateTime Baujahr { get; set; }
+
+        public GetCarsByBaujahrQuery(DateTime baujahr)
+        {
+            Baujahr = baujahr;
+        }
+    }
+}
---
+++ Spg.AutoTeileShop/src/Spg.AutoTeileShop.Repository2/CustomGenericRepositories/CarRepositoryCustom.cs
@@ -0,0 +1,43 @@
+﻿using Spg.AutoTeileShop.Domain.Models;
+using Spg.AutoTeileShop.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.AutoTeileShop.Repository2.CustomGenericRepositories
+{
+    public class CarRepositoryCustom : ReadOnlyRepositoryBase<Car>
+    {
+        private AutoTeileShopContext _db { get; set; }
+        public CarRepositoryCustom(AutoTeileShopContext context) : base(context)
+        {
+            _db = context;
+        }
+        public IEnumerable<Car> GetByBauJahr(DateTime baujahr)
+        {
+            return _db.Cars.Where(c => c.Baujahr.Year == baujahr.Year).ToList();
+        }
+
+        public IEnumerable<Car> GetByMarke(string marke)
+        {
+            return _db.Cars.Where(c => c.Marke == marke).ToList();
+        }
+        public IEnumerable<Car> GetByModell(string model)
+        {
+            return _db.Cars.Where(c => c.Modell == model).ToList();
+        }
+
+        public IEnumerable<Car> GetByMarkeAndModell(string marke, string model)
+        {
+            return _db.Cars.Where(c => c.Marke == marke && c.Modell == model).ToList();
+        }
+
+        public IEnumerable<Car> GetByMarkeAndModellAndBaujahr(string marke, string model, DateTime baujahr)
+        {
+            return _db.Cars.Where(c => c.Marke == marke && c.Modell == model && c.Baujahr.Year == baujahr.Year).ToList();
+        }
+
+    }
+}
