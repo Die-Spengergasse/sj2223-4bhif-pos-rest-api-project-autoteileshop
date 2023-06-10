fdd13b64314cbdfbb583836878a31e407597bfa2
hannesongit <sch20371@spengergasse.at>
2023-06-10 15:04:09 +0200

    Update NuGet Packages

--- Spg.AutoTeileShop/tests/Spg.AutoTeileShop.ApplicationTest/Helpers/TestServiceProvider.cs
+++ Spg.AutoTeileShop/tests/Spg.AutoTeileShop.ApplicationTest/Helpers/TestServiceProvider.cs
@@ -93,8 +93,10 @@ namespace Spg.AutoTeileShop.ApplicationTest.Helpers
         {
             DbContextOptions options = new DbContextOptionsBuilder()
                   //.UseSqlite("Data Source=AutoTeileShopTest.db")
-                  .UseSqlite(@"Data Source= D:/4 Klasse/Pos1 Repo/sj2223-4bhif-pos-rest-api-project-autoteileshop/Spg.AutoTeileShop/src/AutoTeileShop.db")      //Laptop
-                  //.UseSqlite(ReadLineWithQuestionMark())     //Home PC       
+                  .UseSqlite("DataSource= I:\\Dokumente 4TB\\HTL\\4 Klasse\\POS1 Git Repo\\sj2223-4bhif-pos-rest-api-project-autoteileshop\\Spg.AutoTeileShop\\src\\Spg.AutoTeileShop.API\\dbAutoTeileShop.db")
+                  
+                //.UseSqlite(@"Data Source= D:/4 Klasse/Pos1 Repo/sj2223-4bhif-pos-rest-api-project-autoteileshop/Spg.AutoTeileShop/src/AutoTeileShop.db")      //Laptop
+                //.UseSqlite(ReadLineWithQuestionMark())     //Home PC       
                 .Options;
 
             AutoTeileShopContext db = new AutoTeileShopContext(options);
